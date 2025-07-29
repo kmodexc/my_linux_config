bac63e817db8a78f324248e06e31de599186ebcf
marius schlueter <themrslue@googlemail.com>
2025-07-29 19:50:27 +0200

    fix

--- import.sh
+++ import.sh
@@ -3,5 +3,6 @@
 echo "This script will update existing files in both locations"
 
 rsync -avz --update --existing /usr/local/bin/ ./data/
-rsync -avz --update --existing ~/.config/i3/ ./data/
+rsync -avz --update --existing ~/.config/i3/ ./data/i3/
+rsync -avz --update --existing ~/.config/sway/ ./data/sway/
 rsync -avz --update --existing ~ ./data/
